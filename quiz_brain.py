428286dd40a793cb3bd99bf090aee5aae8ae2cdf
Michael Peralta <peralta.michael27@gmail.com>
2023-12-26 01:04:57 -0500

    Creating quiz brain class adding attributes and next question method

---
+++ quiz_brain.py
@@ -0,0 +1,17 @@
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        answer = input(f"Q.{self.question_number}: {current_question.text}. (True/False)?: ")
+
+# TODO: 1. Asking the question
+
+
+# TODO: 2. Checking if the answer was correct
+
+
+# TODO: 3. Checking if we're the end of the quiz
